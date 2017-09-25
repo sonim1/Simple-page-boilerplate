8bfd6c08bc82bed0956aa04b40f02674987f9a84
sonim1 <bumfoo@gmail.com>
2017-09-25 21:46:28 +0900

    Create Simple Boilerplage

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+});
+
+const extractSass = new ExtractTextPlugin({
+    filename: './style/[name].[contenthash].css',
+});
+
+module.exports = {
+    entry: [
+        './src/index.js',
+        './src/style/main.scss',
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /.js?$/,
+            include: [
+                path.resolve(__dirname, 'src'),
+            ],
+            exclude: [
+                path.resolve(__dirname, 'node_modules'),
+                path.resolve(__dirname, 'bower_components'),
+            ],
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015'],
+            },
+        },
+        {
+            test: /\.(css|sass|scss)$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: 'css-loader',
+                }, {
+                    loader: 'sass-loader',
+                }],
+                // use style-loader in development
+                fallback: 'style-loader',
+            }),
+        },
+        {
+            test: /\.(jpg|png|gif|svg)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: 'images/[name].[ext]',
+                    publicPath: '../',
+                },
+            },
+        }],
+    },
+    resolve: {
+        extensions: ['.json', '.js', '.jsx', '.css'],
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        htmlWebpackPlugin,
+        extractSass,
+    ],
+};
